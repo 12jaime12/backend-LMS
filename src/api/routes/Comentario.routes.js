8197dfec774419f217330873f94fd8c5aa90d065
Samuelson23 <3samuelmg@gmail.com>
2023-06-19 09:30:22 +0200

    comentario

--- src/api/routes/Comentario.routes.js
+++ src/api/routes/Comentario.routes.js
@@ -9,8 +9,8 @@ const {
 const { isAuthClient } = require("../../middleware/auth.middleware");
 const ComentariosRoutes = express.Router();
 
-ComentariosRoutes.post("/create", [isAuthClient], createComent);
-ComentariosRoutes.delete("/delete", [isAuthClient], deleteComent);
+ComentariosRoutes.post("/create/:id", [isAuthClient], createComent);
+ComentariosRoutes.delete("/delete/:id", [isAuthClient], deleteComent);
 ComentariosRoutes.get("/getAll", [isAuthClient], getAll);
 ComentariosRoutes.get("/getByCoche/:id", [isAuthClient], getByCoche);
 ComentariosRoutes.get("/getByCatalogo/:id", [isAuthClient], getByCatalogo);
